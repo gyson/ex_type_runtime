b8dfccfb3f0306b1d27a1613387c94e033d2b722
gyson <eilian.yunsong@gmail.com>
2019-04-22 18:56:45 -0400

    Update doc and package

--- lib/t.ex
+++ lib/t.ex
@@ -1,6 +1,35 @@
 defmodule T do
   @moduledoc """
-  Minimal runtime support for ExType
+
+  Minimal runtime support for [`ExType`](https://github.com/gyson/ex_type).
+
+  It brings serveral useful typespecs and macros to
+
+  ### Generic Protocol
+
+      @spec map(T.p(Enumerable, x), (x -> y)) :: [y] when x: any(), y: any()
+
+  Ideally, this should be `Enumerable.t(x)` instead, but `Enumerable.t/1` does not exist.
+  Therefore, use `T.p(Enumerable, x)` to simulate it.
+
+  ### Intersection Type Support
+
+      @type intersection(protocol_1, protocol_2) :: T.&({protocol_1, protocol_2})
+
+  ### Type Assertion
+
+      x = 123
+
+      # left side is expression, right side is typespec
+      T.assert(x == integer())
+
+  ### Type Inspection
+
+      x = 123
+
+      # print the type of `x` when using ex_type
+      T.inspect x
+
   """
 
   # usage: T.&({a, b, c, d})
@@ -21,8 +50,8 @@ defmodule T do
 
   """
 
-  defmacro inspect(x, _opts \\ []) do
-    quote(do: unquote(x))
+  defmacro inspect(expr, _opts \\ []) do
+    quote(do: unquote(expr))
   end
 
   @doc """
--- mix.exs
+++ mix.exs
@@ -5,9 +5,13 @@ defmodule T.MixProject do
     [
       app: :t,
       version: "0.1.0",
+      description: "Minimal runtime support for ExType",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      package: package(),
+      deps: deps(),
+      name: "T",
+      source_url: "https://github.com/gyson/t"
     ]
   end
 
@@ -21,8 +25,14 @@ defmodule T.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.20.2", only: :dev, runtime: false},
     ]
+  end
+
+  def package do
+    %{
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/gyson/t"}
+    }
   end
 end
