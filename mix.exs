e0f272abd78528cfc284c2dc523e76437c3e4260
gyson <eilian.yunsong@gmail.com>
2019-09-14 11:18:26 -0400

    Bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExTypeRuntime.MixProject do
   def project do
     [
       app: :ex_type_runtime,
-      version: "0.1.0",
+      version: "0.2.0",
       description: "Minimal runtime support for ExType",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
